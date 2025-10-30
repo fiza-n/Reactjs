c448d28a9575f24910695fce936b9683b9326acd
fiza noor <fnoor034726@gmail.com>
2025-10-31 00:23:07 +0500

    'Currency Convertor'

---
+++ CurrencyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+
+import InputBox from "./components/InputBox"
+import useCurrencyInfo from "./hooks/useCurrencyInfo"
+
+function App() {
+  
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useEffect,useState } from "react"; 
+
+function useCurrencyInfo(currency){//custom hook accepting currency as parameter
+const [data,setData] = useState({});
+
+    useEffect(()=>{//calling api here
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=> res.json())//converting into json
+        .then((res) => setData(res[currency]))//setting data as an object
+        .catch((err) => console.log(err))
+       
+    },[currency])//dependency on currency
+    return data;//returning the api data
+} 
+export default useCurrencyInfo;

419669bf164a9d6db6845b656db11cd9c4db4e24
fiza noor <fnoor034726@gmail.com>
2025-10-30 20:03:59 +0500

    'complete the password generator'

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState,useRef } from 'react'
 
 
 
@@ -13,31 +13,60 @@ function App() {
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if(includeNumbers) str+= "0123456789"
       if(includeCharacters) str+= "!@#$%^&*()_+~`|}{[]:;?><,./-="
-    for(let i=1; i<=str.length; i++){
-      let char =Math.floor(Math.random() * str.length +1);
-      pass= str.charAt(char)
+    for(let i=1; i<=length; i++){
+      let char = Math.floor(Math.random() * str.length +1);
+      pass += str.charAt(char)
     }
     
 
 setPassword(pass);
 
   },[length,includeNumbers,includeCharacters,setPassword])
+
+  const copypasswordtoClipboard= useCallback(()=>{
+    Reference.current?.select();
+    window.navigator.clipboard.writeText(password);
+  },[password])
+
+  useEffect(()=>{
+    passwordGenerator();
+  },[length,includeCharacters,includeNumbers,passwordGenerator])
+
+  const Reference = useRef(null);
  return (
  <section className='w-full h-screen bg-[#101010] flex  justify-center items-center'>
   <div className='flex justify-center flex-col p-6 items-center rounded-3xl bg-gray-700'>
     <h2 className='font-bold text-2xl text-white'>Password Generator</h2>
     <div className='flex justify-between items-center '>
-      <input type="text" className='p-2 mt-2 w-[100%] border rounded-xl border-black' />
-      <button className='bg-blue-500 p-2 m-2 text-white font-bold'>Copy</button>
+      <input type="text" value={password} readOnly
+       ref={Reference} className='p-2 mt-2 w-full border rounded-xl border-black' />
+      <button onClick={copypasswordtoClipboard} className='bg-blue-500 p-2 m-2 text-white font-bold'>Copy</button>
     </div>
     <div className='flex gap-2.5'>
-      
-      <input type="range" className='p-2 m-2 w-full border border-black' />
-      <p>Length(16)</p>
-      <input type="checkbox" className='w-[50px]' />
-      <p>Numbers</p>
-      <input type="checkbox" className='w-[50px]' />
-      <p>Characters</p>
+      <div className='flex justify-center items-center'>
+        <input type="range"
+         min={6}
+          max={50}
+          onChange={(e)=>{setLength(e.target.value)}}
+            value={length}
+           className='p-2 m-2  border border-black'
+           />
+        <label>Length:{length}</label>
+      </div>
+      <div className='flex justify-center items-center'>
+        <input type="checkbox"
+          id='numbers'
+        defaultChecked={includeNumbers} 
+        onChange={()=>{setIncludeNumbers((prev)=>!prev)}}/>
+      <label htmlFor='numbers'>Numbers</label>
+      </div>
+      <div className='flex justify-center items-center'>
+        <input type="checkbox"
+         id='characters'
+          defaultChecked={includeCharacters}
+           onChange={()=>{setIncludeCharacters((prev)=>! prev)}} />
+      <label htmlFor='characters'>Characters</label>
+      </div>
 
     </div>
   </div>

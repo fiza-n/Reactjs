4cfac60b48faa2864e87d793bb41182622e3f350
fiza noor <fnoor034726@gmail.com>
2025-11-06 00:19:56 +0500

    'little change'

--- ContextApi1/src/components/Profile.jsx
+++ ContextApi1/src/components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = () => {
  const {user} =useContext(UserContext);
 
  if(!user) return <div>Please Login!</div>
- return <div>Welcome {user.username} {user.passwrod}</div>
+ return <div>Welcome {user.username} </div>
   
 }
 
